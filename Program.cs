caa366e71ee81680814768b88a14a600b727f4fc
Mark Bussell Jr <webdev@waywardsondevelopers.com>
2017-01-16 14:12:49 -0500

    Read #3, building some examples

--- Program.cs
+++ Program.cs
@@ -7,11 +7,18 @@ namespace ConsoleApplication
         public static void Main()
         {
             //Tutorial Version:
-            /*string userName = "Test";
-            Console.WriteLine($"Hello {userName}!"); */
+            string userName = "Tutorial";
+            Console.WriteLine($"{userName} is a go:");
 
             //Version I see elsewhere:
-            Console.WriteLine("Hello {0}!", "Test");
+            //Console.WriteLine("Hello {0}!", "Test");
+
+            //String Ops Used in the Tutorial:
+            string emptyString = String.Empty;
+            string nullString = null;
+
+            Console.WriteLine("\"\" == String.Empty is {0}", "" == String.Empty); //Don't forget to escape the quotes...
+            Console.WriteLine("nullString == null is {0}", nullString == null); //Wouldn't let me just have "string nullString;" - error CS0165: Use of unassigned local variable 'nullString'
         }
     }
 }
